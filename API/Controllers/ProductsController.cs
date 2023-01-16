fed9127668dff7b97ab6903dc3bcc7091077e5f8
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-17 04:36:45 +0530

    added repositoies and data fetching

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,28 +1,29 @@
 using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
     public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _productRepo;
+        public ProductsController(IProductRepository productRepo)
         {
-            _context = context;
+            _productRepo = productRepo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            return Ok(await _productRepo.GetProductsAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _productRepo.GetProductByIdAsync(id);
         }
     }
 }
