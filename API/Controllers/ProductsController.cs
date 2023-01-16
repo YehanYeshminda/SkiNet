f7b827d9eab391dbab8673daaa9f58af2888153c
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-17 04:27:07 +0530

    added the interfaces and repos

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -14,13 +14,13 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts() 
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
             return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id) 
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _context.Products.FindAsync(id);
         }
