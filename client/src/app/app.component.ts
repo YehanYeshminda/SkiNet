ddaa44e920606c3b48fb6337571a3ac8e089f275
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-21 16:27:27 +0530

    added the interfaces for pagination and products and data fetch

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,10 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   title = 'SkiNet';
-  products: any = []
+  products: IProduct[];
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/Products?pageSize=50').subscribe((response: any) => {
+    this.http.get('https://localhost:5001/api/Products?pageSize=50').subscribe((response: IPagination) => {
       this.products = response.data;
     });
   }
