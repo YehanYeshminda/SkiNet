581a8f7dcb4010177aef21dd67c0484d88b760e9
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-21 19:15:41 +0530

    added the component for pagination

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 
