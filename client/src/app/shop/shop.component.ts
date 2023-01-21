86aa78cab7f7edbef0e19843bb82985dc0cfc855
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-21 22:45:58 +0530

    refactored some code

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -82,11 +82,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts(); // this will fetch the brands selected;
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -96,12 +98,15 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch() {
     this.shopParams.search = this.searchInput.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
