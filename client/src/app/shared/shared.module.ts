6b1298ce23e3d50358063d58adffc4c91ef9f0bb
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-21 19:48:54 +0530

    added the pagination header as a reuable component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PaginationHeaderComponent } from './components/pagination-header/pagination-header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaginationHeaderComponent
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
   ],
   exports: [
-    PaginationModule
+    PaginationModule,
+    PaginationHeaderComponent,
   ]
 })
 export class SharedModule { }
