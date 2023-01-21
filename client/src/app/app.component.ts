b0d2245f41148b7938c55d31b823307a04b96722
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-21 16:21:28 +0530

    added nav and simple data fetching

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  title = 'SkiNet';
+  products: any = []
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/Products?pageSize=50').subscribe((response: any) => {
+      this.products = response.data;
+    });
+  }
 }
