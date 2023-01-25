77f63acd77a8a7c8083bc2e0ef45ff500ada41ab
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-25 07:41:10 +0530

    added the carousel

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,21 +3,24 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PaginationHeaderComponent } from './components/pagination-header/pagination-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
 @NgModule({
   declarations: [
     PaginationHeaderComponent,
-    PagerComponent
+    PagerComponent,
   ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PaginationHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule,
   ]
 })
 export class SharedModule { }
