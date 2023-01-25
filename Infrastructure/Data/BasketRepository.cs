7b2b3817ba545c7553ed719c7d7dc27b840a3477
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-25 08:17:34 +0530

    added the basket repository methods

--- Infrastructure/Data/BasketRepository.cs
+++ Infrastructure/Data/BasketRepository.cs
@@ -1,23 +1,41 @@
+using System.Text.Json;
 using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
 
 namespace Infrastructure.Data
 {
     public class BasketRepository : IBasketRepository
     {
-        public Task<bool> DeleteBasketAsync(string basketId)
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
         {
-            throw new NotImplementedException();
+            _database = redis.GetDatabase(); // setting up the connection to redis database
         }
 
-        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        // in order to delete the basket
+        public async Task<bool> DeleteBasketAsync(string basketId)
         {
-            throw new NotImplementedException();
+            return await _database.KeyDeleteAsync(basketId);
         }
 
-        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
         {
-            throw new NotImplementedException();
+            var data = await _database.StringGetAsync(basketId);
+
+            // deserializing the data into the customer basket entity
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
+        }
+
+        // update or create
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var created = await _database.StringSetAsync(basket.Id, 
+                JsonSerializer.Serialize(basket), TimeSpan.FromHours(3)); // keeps the basket for 3 hours or can be kept for days as well
+
+            if (!created) return null;
+
+            return await GetBasketAsync(basket.Id);
         }
     }
 }
