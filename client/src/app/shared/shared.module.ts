eeeb8192cbba19ec5c86d1d98b3ece4e06df5381
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-01-26 08:45:30 +0530

    made a resuable function to show the order totals

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PaginationHeaderComponent } from './components/pagination-header/pagination-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 
 
 @NgModule({
   declarations: [
     PaginationHeaderComponent,
     PagerComponent,
+    OrderTotalsComponent,
   ],
   imports: [
     CommonModule,
@@ -21,6 +23,7 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
     PaginationHeaderComponent,
     PagerComponent,
     CarouselModule,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule { }
