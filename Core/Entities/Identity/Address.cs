dfe8610edf3133d9b448022f4f92bda2c8a4047c
Yehan Yeshminda <yeshanyesh12@gmail.com>
2023-01-27 22:20:33 +0530

    added the indentity framework config step 1

---
+++ Core/Entities/Identity/Address.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        [Required] // removed the field from being nullable
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; } // one user can have one address
+    }
+}
